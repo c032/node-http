d08d8932cf3908b7ac817e1eebfaea7b391253fe
c032 <c032+github@wark.io>
2021-07-12 01:00:23 +0000

    Move `examples` outside `src`

--- examples/fileserver/main.js
+++ examples/fileserver/main.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-import { Server } from '../../server.js';
+import { Server } from '../../src/server.js';
 
 import {
     errorWrapperHandler,
