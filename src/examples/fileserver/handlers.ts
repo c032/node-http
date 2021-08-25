23452e76e86a649ae1a2f616452a09f0e00723e1
c032 <c032+github@wark.io>
2021-08-25 16:45:42 +0000

    Fix ESLint

--- src/examples/fileserver/handlers.ts
+++ src/examples/fileserver/handlers.ts
@@ -8,19 +8,19 @@ import {
 } from '../../server';
 
 function serveFile(ctx: IContext, filePath: string): void {
-    ctx.response.status = 200;
+	ctx.response.status = 200;
 
-    // TODO: Guess file type from extension.
-    ctx.response.setHeader('Content-Type', 'application/octet-stream');
+	// TODO: Guess file type from extension.
+	ctx.response.setHeader('Content-Type', 'application/octet-stream');
 
-    ctx.response.body = fs.createReadStream(filePath);
+	ctx.response.body = fs.createReadStream(filePath);
 }
 
 async function serveDirectory(ctx: IContext, directoryPath: string): Promise<void> {
-    let requestPath = ctx.request.path;
-    if (!requestPath.endsWith('/')) {
-        requestPath += '/';
-    }
+	let requestPath = ctx.request.path;
+	if (!requestPath.endsWith('/')) {
+		requestPath += '/';
+	}
 
 	interface IEntry {
 		type: string;
@@ -28,134 +28,137 @@ async function serveDirectory(ctx: IContext, directoryPath: string): Promise<voi
 		absolutePath: string;
 	}
 
-    const responseBody = {
-        entries: [] as IEntry[],
-    };
+	const responseBody = {
+		entries: [] as IEntry[],
+	};
 
-    const rawEntries = await fs.promises.readdir(directoryPath, {
-        withFileTypes: true,
-    });
+	const rawEntries = await fs.promises.readdir(directoryPath, {
+		withFileTypes: true,
+	});
 
-    for (const rawEntry of rawEntries) {
-        const { name } = rawEntry;
-        if (name.startsWith('.')) {
-            continue;
-        }
+	for (let i = 0; i < rawEntries.length; i += 1) {
+		const rawEntry = rawEntries[i];
+		if (!rawEntry) {
+			continue;
+		}
 
-        let type;
-        if (rawEntry.isDirectory()) {
-            type = 'directory';
-        } else if (rawEntry.isFile()) {
-            type = 'file';
-        } else if (rawEntry.isSymbolicLink()) {
-            // TODO: Recursively find the type of the symlink target.
-            //
-            // `type` should only be either `file` or `directory`.
-            type = 'symlink';
-        } else {
-            continue;
-        }
+		const { name } = rawEntry;
+		if (name.startsWith('.')) {
+			continue;
+		}
 
-        let urlPath = requestPath + name;
-        if (type === 'directory') {
-            urlPath += '/';
-        }
+		let type: 'directory' | 'file' | 'symlink';
+		if (rawEntry.isDirectory()) {
+			type = 'directory';
+		} else if (rawEntry.isFile()) {
+			type = 'file';
+		} else if (rawEntry.isSymbolicLink()) {
+			// TODO: Recursively find the type of the symlink target.
+			//
+			// `type` should only be either `file` or `directory`.
+			type = 'symlink';
+		} else {
+			continue;
+		}
 
-        const entry: IEntry = {
-            type,
-            name,
-            absolutePath: urlPath,
-        };
+		let urlPath = requestPath + name;
+		if (type === 'directory') {
+			urlPath += '/';
+		}
 
-        responseBody.entries.push(entry);
-    }
+		const entry: IEntry = {
+			type,
+			name,
+			absolutePath: urlPath,
+		};
 
-    responseBody.entries.sort((a, b) => a.name.localeCompare(b.name));
+		responseBody.entries.push(entry);
+	}
 
-    ctx.response.json(responseBody);
+	responseBody.entries.sort((a, b) => a.name.localeCompare(b.name));
+
+	ctx.response.json(responseBody);
 }
 
 export async function requestLoggerHandler(ctx: IContext, next: INextFunction): Promise<void> {
-    const start = Date.now();
-    const startTimestamp = JSON.parse(
-        JSON.stringify(new Date(start))
-    );
+	const start = Date.now();
+	const startTimestamp = JSON.parse(
+		JSON.stringify(new Date(start)),
+	);
 
-    const {
-        method,
-        path,
-    } = ctx.request;
+	// eslint-disable-next-line no-console
+	console.log(`--> ${startTimestamp} ${ctx.request.method} ${ctx.request.path}`);
 
-    console.log(`--> ${startTimestamp} ${method} ${path}`);
+	await next();
 
-    await next();
+	const end = Date.now();
+	const endTimestamp = JSON.parse(
+		JSON.stringify(new Date(end)),
+	);
 
-    const end = Date.now();
-    const endTimestamp = JSON.parse(
-        JSON.stringify(new Date(end))
-    );
+	const diff = end - start;
 
-    const diff = end - start;
-
-    console.log(`<-- ${endTimestamp} ${method} ${path} (${diff}ms)`);
+	// eslint-disable-next-line no-console
+	console.log(`<-- ${endTimestamp} ${ctx.request.method} ${ctx.request.path} (${diff}ms)`);
 }
 
-export function staticFileHandler(root: string): IHandler {
-    return async function staticFileHandler(ctx: IContext, next: INextFunction) {
-        const { request } = ctx;
-        const urlPath = request.path;
+export function staticFileHandlerFactory(root: string): IHandler {
+	return async function staticFileHandler(ctx: IContext, next: INextFunction): Promise<void> {
+		const { request } = ctx;
+		const urlPath = request.path;
 
-        let relativePath;
-        if (urlPath === '/') {
-            relativePath = '.';
-        } else {
-            relativePath = urlPath.substring(1);
-        }
+		let relativePath;
+		if (urlPath === '/') {
+			relativePath = '.';
+		} else {
+			relativePath = urlPath.substring(1);
+		}
 
-        const absolutePath = path.join(root, relativePath);
+		const absolutePath = path.join(root, relativePath);
 
-        const stat = await fs.promises.stat(absolutePath);
-        if (stat.isDirectory()) {
-            if (!urlPath.endsWith('/')) {
-                ctx.response.status = 303;
-                ctx.response.setHeader('Location', `${urlPath}/`);
+		const stat = await fs.promises.stat(absolutePath);
+		if (stat.isDirectory()) {
+			if (!urlPath.endsWith('/')) {
+				ctx.response.status = 303;
+				ctx.response.setHeader('Location', `${urlPath}/`);
 
-                return;
-            }
+				return Promise.resolve();
+			}
 
-            return serveDirectory(ctx, absolutePath);
-        } else if (stat.isFile()) {
-            return serveFile(ctx, absolutePath);
-        } else {
-            return next();
-        }
-    };
+			return serveDirectory(ctx, absolutePath);
+		} if (stat.isFile()) {
+			return serveFile(ctx, absolutePath);
+		}
+
+		return next();
+	};
 }
 
 export async function notFoundHandler(ctx: IContext): Promise<void> {
-    ctx.response.status = 404;
-    ctx.response.json({
-        error: {
-            message: 'Not found.',
-        },
-    });
+	ctx.response.status = 404;
+	ctx.response.json({
+		error: {
+			message: 'Not found.',
+		},
+	});
 }
 
 export async function errorWrapperHandler(ctx: IContext, next: INextFunction): Promise<void> {
-    try {
-        await next();
-    } catch (err) {
-        console.error(err.stack);
+	try {
+		await next();
+	} catch (err) {
+		// eslint-disable-next-line no-console
+		console.error(err.stack);
 
-        ctx.response.status = 500;
-        ctx.response.json({
-            error: {
-                message: 'Internal server error.',
-                debug: {
-                    message: err.message,
-                    stack: err.stack,
-                },
-            },
-        });
-    }
+		ctx.response.status = 500;
+		ctx.response.json({
+			error: {
+				message: 'Internal server error.',
+				debug: {
+					message: err.message,
+					stack: err.stack,
+				},
+			},
+		});
+	}
 }
--- src/examples/fileserver/main.ts
+++ src/examples/fileserver/main.ts
@@ -3,22 +3,22 @@ import * as path from 'path';
 import { Server } from '../../server';
 
 import {
-    errorWrapperHandler,
-    notFoundHandler,
-    requestLoggerHandler,
-    staticFileHandler,
+	errorWrapperHandler,
+	notFoundHandler,
+	requestLoggerHandler,
+	staticFileHandlerFactory,
 } from './handlers';
 
 function main() {
-    const root = path.join(__dirname, '..');
-    const s = new Server();
+	const root = path.join(__dirname, '..');
+	const s = new Server();
 
-    s.pushHandler(requestLoggerHandler);
-    s.pushHandler(errorWrapperHandler);
-    s.pushHandler(staticFileHandler(root));
-    s.pushHandler(notFoundHandler);
+	s.pushHandler(requestLoggerHandler);
+	s.pushHandler(errorWrapperHandler);
+	s.pushHandler(staticFileHandlerFactory(root));
+	s.pushHandler(notFoundHandler);
 
-    s.listen(3000);
+	s.listen(3000);
 }
 
 main();
